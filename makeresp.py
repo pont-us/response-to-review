c72dec94ff4f1608239a31a9168b80505350615b
Pontus Lurcock <pont@talvi.net>
2020-02-28 16:33:08 +0100

    Clean up Python code
    makeresp.py: refactor, improve code style, call Python 3 via env
    in the shebang line.

--- makeresp.py
+++ makeresp.py
@@ -1,39 +1,49 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
-'''Produce LaTeX source from custom comment-response markup.
-'''
+"""Produce LaTeX source from custom comment-response markup.
+"""
 
-import argparse
-argp = argparse.ArgumentParser()
-argp.add_argument("--input", help="input file name")
-argp.add_argument("--output", help="output file name")
-args = argp.parse_args()
 
-with open(args.input, 'r') as ifh, open(args.output, 'w') as ofh:
-    cell_lines, cell_comments, cell_response = '','',''
+def main():
+    import argparse
+    argp = argparse.ArgumentParser()
+    argp.add_argument("--input", help="input file name")
+    argp.add_argument("--output", help="output file name")
+    args = argp.parse_args()
+    with open(args.input, "r") as ifh, open(args.output, "w") as ofh:
+        process_file(ifh, ofh)
+
+
+def process_file(ifh, ofh):
+    cell_lines, cell_comments, cell_response = "", "", ""
     part = 0
     first = True
     lines = ifh.readlines()
     lines.append(None)
     for line in lines:
-        if line != None: line = line.strip()
-        if line==None or line.startswith('c '):
+        if line is not None:
+            line = line.strip()
+        if line is None or line.startswith("c "):
             if not first:
-                if cell_response == '':
-                    cell_response = 'Done.'
-                ofh.write('\\raggedright\\it ' + cell_lines + ' &\n')
-                ofh.write(cell_comments + '&\n')
-                ofh.write(cell_response + ' \\\\\n')
-                cell_lines, cell_comments, cell_response = '','',''
-            if line != None:
+                if cell_response == "":
+                    cell_response = "Done."
+                ofh.write("\\raggedright\\it " + cell_lines + " &\n")
+                ofh.write(cell_comments + "&\n")
+                ofh.write(cell_response + " \\\\\n")
+                cell_lines, cell_comments, cell_response = "", "", ""
+            if line is not None:
                 cell_lines = line[2:]
                 part = 1
         elif part == 1:
-            if line.startswith('r '):
+            if line.startswith("r "):
                 cell_response = line[2:]
                 part = 2
             else:
-                cell_comments += ' ' + line
+                cell_comments += " " + line
         elif part == 2:
-            cell_response += ' ' + line
+            cell_response += " " + line
         first = False
+
+
+if __name__ == "__main__":
+    main()
